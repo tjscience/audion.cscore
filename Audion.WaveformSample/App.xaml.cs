e794a928e084b527b91a130e6c03b44c232ff8c1
tjscience <terry.w.phillips@gmail.com>
2017-02-08 15:24:32 -0500

    Changed the Sample app's name

--- Audion.WaveformSample/App.xaml.cs
+++ Audion.WaveformSample/App.xaml.cs
@@ -5,7 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Windows;
 
-namespace Audion.WaveformSample
+namespace Audion.Sample
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- Audion.WaveformSample/MainWindow.xaml.cs
+++ Audion.WaveformSample/MainWindow.xaml.cs
@@ -3,7 +3,7 @@ using System;
 using System.Windows;
 using System.Windows.Media;
 
-namespace Audion.WaveformSample
+namespace Audion.Sample
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
