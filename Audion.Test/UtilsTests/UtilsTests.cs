fd72c99b8c30dfd97a22cc50b80f4342166c1f64
tjscience <terry.w.phillips@gmail.com>
2017-03-18 14:10:09 -0400

    Troubleshooting tests

---
+++ Audion.Test/UtilsTests/UtilsTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace Audion.Test.UtilsTests
+{
+    [TestClass]
+    public class UtilsTests
+    {
+        [TestMethod]
+        public void WhiteNoiseSampleMP3Test()
+        {
+            var path = Utils.WhiteNoiseSampleMP3;
+            Assert.IsTrue(File.Exists(path), "does not exist: " + path);
+        }
+
+        [TestMethod]
+        public void VideoSampleMP4Test()
+        {
+            var path = Utils.VideoSampleMP4;
+            Assert.IsTrue(File.Exists(path), "does not exist: " + path);
+        }
+    }
+}
