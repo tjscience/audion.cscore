77c7393f3473777a954a5be4d69e436ea8ddb749
tjscience <terry.w.phillips@gmail.com>
2017-05-12 17:00:49 -0400

    fixed Font Awesome issue

--- Audion.Sample/Properties/AssemblyInfo.cs
+++ Audion.Sample/Properties/AssemblyInfo.cs
@@ -3,6 +3,7 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Markup;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -53,3 +54,7 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.8.2")]
 [assembly: AssemblyFileVersion("0.8.2")]
+
+[assembly: XmlnsPrefix("http://schemas.fontawesome.io/icons/", "fa")]
+[assembly: XmlnsDefinition("http://schemas.fontawesome.io/icons/", "FontAwesome.WPF")]
+[assembly: XmlnsDefinition("http://schemas.fontawesome.io/icons/", "FontAwesome.WPF.Converters")]
